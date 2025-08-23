b70855d15e9a28ccb1b2fbb261023eae5a08693c
Suhee231231 <147281452+Suhee231231@users.noreply.github.com>
2025-08-24 03:41:37 +0900

    데이터베이스 연결 방식을 개별 환경 변수로 변경

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -2,7 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
