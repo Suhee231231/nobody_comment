60ce7e061aede449d2f299f557968b1896254443
Suhee231231 <147281452+Suhee231231@users.noreply.github.com>
2025-08-24 15:53:37 +0900

    캐싱 문제 해결: Service Worker 제거 및 설정 단순화

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -13,32 +13,6 @@ root.render(
   </React.StrictMode>
 );
 
-// Service Worker 등록
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('SW 등록 성공:', registration.scope);
-        
-        // Service Worker 업데이트 확인
-        registration.addEventListener('updatefound', () => {
-          const newWorker = registration.installing;
-          if (newWorker) {
-            newWorker.addEventListener('statechange', () => {
-              if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-                // 새 Service Worker가 설치되었을 때 페이지 새로고침
-                window.location.reload();
-              }
-            });
-          }
-        });
-      })
-      .catch((registrationError) => {
-        console.log('SW 등록 실패:', registrationError);
-      });
-  });
-}
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
